4a35d75f34566c72b85516f65509b19f38eeadf4
DanielShanklin501 <80490716+DanielShanklin501@users.noreply.github.com>
2021-03-11 17:45:48 -0500

    commit 2... fixed country alias

--- TestSQL1.sql
+++ TestSQL1.sql
@@ -2,8 +2,8 @@ select
 	country.country_name_eng,
 	sum(case when call.id is null then 1 else 0 end) as calls,
 	avg(isnull(datediff(second, call.start_time, call.end_time),0)) as avg_difference
-from country 
-left join citystates on city.country_id = country.id
+from countrycodes AS country 
+left join citystatescountries on city.country_id = country.id
 left join customer on city.id = customer.city_id
 left join call on call.customer_id = customer.id
 group by 
@@ -12,4 +12,4 @@ group by
 having avg(isnull(datediff(second, call.start_time, call.end_time_ampm),0)) > (select avg(datediff(second, call.start_time, call.end_time)) from call)
 order by calls desc, country.id asc;
 
--- comment
+-- comment 2
